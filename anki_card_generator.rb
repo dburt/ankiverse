9d6e37756748f1fd0ebbdb8b6a04a8193798bfc3
Dave Burt <dave@burt.id.au>
2012-12-04 23:58:43 +1100

    Refactor card generation into its own class.

---
+++ anki_card_generator.rb
@@ -0,0 +1,31 @@
+
+class AnkiCardGenerator
+  attr_accessor :poem, :other_fields
+
+  def initialize(poem, other_fields)
+    @poem = poem.to_s
+    @other_fields = [*other_fields].select {|field| field.to_s != "" }
+  end
+
+  def lines
+    @lines ||= begin
+      iconv = Iconv.new('UTF-8', 'WINDOWS-1252')
+      poem.strip.split(/\r?\n/).map do |line|
+        iconv.iconv line.strip
+      end
+    end
+  end
+
+  def csv(options = {})
+    # future options: :ellipsis, :lines
+    FasterCSV.generate do |csv|
+      (lines.size - 2).times do |i|
+        csv << [
+          lines[[0, i-3].max..i].join("<br/>") + "<br/>...", # 4 prev lines
+          lines[i + 1, 2].join("<br/>"),                     # 2 next lines
+          *other_fields
+        ]
+      end
+    end
+  end
+end
--- ankiverse.rb
+++ ankiverse.rb
@@ -6,6 +6,7 @@ require 'fastercsv'
 require 'iconv'
 require 'yaml'
 
+require './anki_card_generator'
 require './esv_api_request'
 require './sentence_splitter'
 
@@ -54,32 +55,13 @@ class AnkiVerse < Sinatra::Base
   end
 
   post '/' do
-
     next params.to_yaml if params["debug"]
-
-    iconv = Iconv.new('UTF-8', 'WINDOWS-1252')
-    lines = params["poem"].to_s.strip.split(/\r?\n/)
-    lines.map! do |line|
-      iconv.iconv line.strip
-    end
-
-    other_fields = params["other_fields"]
-    other_fields = [] unless other_fields.kind_of? Array
-    other_fields.delete_if {|field| field.to_s == "" }
 
     response['Content-Type'] = "text/csv; charset=utf-8"
     response['Content-Disposition'] = "attachment; filename=ankiverse.csv"
 
-    FasterCSV.generate do |csv|
-      (lines.size - 2).times do |i|
-        csv << [
-          lines[[0, i-3].max..i].join("<br/>") + "<br/>...", # 4 prev lines
-          lines[i + 1, 2].join("<br/>"),                     # 2 next lines
-          *other_fields
-        ]
-      end
-    end
-
+    AnkiCardGenerator.new(params["poem"], params["other_fields"]).
+      csv(:lines => [4, 2], :ellipsis => true)
   end
 
   get '/bible/:passage' do
